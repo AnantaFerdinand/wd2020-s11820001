38d72e6f9c2e498fc9ca1fff672eddac16c95962
AnantaFerdinand <s11820001@student.unklab.ac.id>
2020-11-09 21:51:28 +0800

    16th commit

---
+++ pertemuan_16/index.js
@@ -0,0 +1,11 @@
+let nama = prompt("Masukkan Nama Anda: ");
+console.log(nama);
+
+
+let berat = prompt("Berat Badan anda adalah = ");
+let tinggi = prompt("Tinggi badan anda adalah = ");
+console.log(berat);
+console.log(tinggi);
+
+let bmi = (100 / berat / (tinggi * tinggi));
+console.log(bmi);
